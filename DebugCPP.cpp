235450e45a87e8d384881e680257f5545693ebea
programmercert <programmercert@gmail.com>
2017-05-09 15:54:39 -0400

    Add files via upload
    The Cpp and header file

---
+++ DebugCPP.cpp
@@ -0,0 +1,79 @@
+#include "DebugCPP.h"
+
+#include<stdio.h>
+#include <string>
+#include <stdio.h>
+#include <sstream>
+
+//-------------------------------------------------------------------
+void  Debug::Log(const char* message, Color color) {
+	if (callbackInstance != nullptr)
+		callbackInstance(message, (int)color, (int)strlen(message));
+}
+
+void  Debug::Log(const std::string message, Color color) {
+	const char* tmsg = message.c_str();
+	if (callbackInstance != nullptr)
+		callbackInstance(tmsg, (int)color, (int)strlen(tmsg));
+}
+
+void  Debug::Log(const int message, Color color) {
+	std::stringstream ss;
+	ss << message;
+	send_log(ss, color);
+}
+
+void  Debug::Log(const char message, Color color) {
+	std::stringstream ss;
+	ss << message;
+	send_log(ss, color);
+}
+
+void  Debug::Log(const float message, Color color) {
+	std::stringstream ss;
+	ss << message;
+	send_log(ss, color);
+}
+
+void  Debug::Log(const double message, Color color) {
+	std::stringstream ss;
+	ss << message;
+	send_log(ss, color);
+}
+
+void Debug::Log(const bool message, Color color) {
+	std::stringstream ss;
+	if (message)
+		ss << "true";
+	else
+		ss << "false";
+
+	send_log(ss, color);
+}
+
+void Debug::send_log(const std::stringstream &ss, const Color &color) {
+	const std::string tmp = ss.str();
+	const char* tmsg = tmp.c_str();
+	if (callbackInstance != nullptr)
+		callbackInstance(tmsg, (int)color, (int)strlen(tmsg));
+}
+//-------------------------------------------------------------------
+
+//Create a callback delegate
+void RegisterDebugCallback(FuncCallBack cb) {
+	callbackInstance = cb;
+
+	//Test Call
+	Debug::Log("Hellow Red", Color::Red);
+	Debug::Log("Hellow Green", Color::Green);
+	Debug::Log("Hellow Blue", Color::Blue);
+	Debug::Log("Hellow Black", Color::Black);
+	Debug::Log("Hellow White", Color::White);
+	Debug::Log("Hellow Yellow", Color::Yellow);
+	Debug::Log("Hellow Orange", Color::Orange);
+
+	Debug::Log("Hellow Default Black Color");
+
+	Debug::Log(true, Color::Black);
+	Debug::Log(false, Color::Red);
+}
---
+++ DebugCPP.h
@@ -0,0 +1,33 @@
+#pragma once
+#include<stdio.h>
+#include <string>
+#include <stdio.h>
+#include <sstream>
+
+#define DLLExport __declspec(dllexport)
+
+extern "C"
+{
+	//Create a callback delegate
+	typedef void(*FuncCallBack)(const char* message, int color, int size);
+	static FuncCallBack callbackInstance = nullptr;
+	DLLExport void RegisterDebugCallback(FuncCallBack cb);
+}
+
+//Color Enum
+enum class Color { Red, Green, Blue, Black, White, Yellow, Orange };
+
+class  Debug
+{
+public:
+	static void Log(const char* message, Color color = Color::Black);
+	static void Log(const std::string message, Color color = Color::Black);
+	static void Log(const int message, Color color = Color::Black);
+	static void Log(const char message, Color color = Color::Black);
+	static void Log(const float message, Color color = Color::Black);
+	static void Log(const double message, Color color = Color::Black);
+	static void Log(const bool message, Color color = Color::Black);
+
+private:
+	static void send_log(const std::stringstream &ss, const Color &color);
+};
